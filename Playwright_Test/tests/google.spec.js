7059031f9c0fd34425b4831430be9614113280aa
sivasai96001 <sivasai96001@gmail.com>
2025-10-01 11:37:39 +0530

    new branch created for allure

--- Playwright_Test/tests/google.spec.js
+++ Playwright_Test/tests/google.spec.js
@@ -3,4 +3,5 @@ import { test, expect } from '@playwright/test'
 test('check title', async({page})=>{
     await page.goto('https://www.google.com/')
     await expect(page).toHaveTitle('Google')
+    
 }) 
